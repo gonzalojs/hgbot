e5f8c82a0e16ba5bb1744f42a0d9f5b75e13cf8a
gonzalojs <eogutier@gmail.com>
2020-06-24 22:08:52 -0400

    first commit

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 require('dotenv').config()
 const Snoowrap = require('snoowrap')
-const { InboxStream, CommentStream, SubmissionStream } = require('snoostorm')
+const {
+  InboxStream,
+  CommentStream,
+  SubmissionStream
+} = require('snoostorm')
 
 const client = new Snoowrap({
-  userAgent: 'reddit-bot-example-node',
+  userAgent: 'harr-ginny-fanfiction-bot',
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
   username: process.env.REDDIT_USER,
   password: process.env.REDDIT_PASS
 })
 
+const BOT_START = Date.now() / 1000
+
+const canSummon = (msg) => {
+  return msg && msg.toLowerCase().includes('/u/hgbot')
+}
+
 const comments = new CommentStream(client, {
-  subreddit: 'news',
-  limit: 100,
-  pollTime: 2000
+  subreddit: 'testingground4bots',
+  limit: 10,
+  pollTime: 10000
 })
-comments.on('item', console.log)
+comments.on('item', (item) => {
+  if (item.created_utc < BOT_START) return
+  if (!canSummon(item.body)) return
+  console.log(item)
+  item.reply('hola mundo!')
+})
