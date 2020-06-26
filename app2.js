2b5058e19aac6b5bb2d9f1b23acfecc5d605e0e3
gonzalojs <eogutier@gmail.com>
2020-06-25 23:07:39 -0400

    chaoters get

--- app2.js
+++ app2.js
@@ -1,25 +1,42 @@
 var Xray = require('x-ray')
 var x = Xray()
 
-x('https://www.fanfiction.net/s/7750443/1/Marauders-Growing-Up', '#content_wrapper_inner', [
-  {
+x('https://www.fanfiction.net/s/7750443/', '#content_wrapper_inner', [{
     title: 'b.xcontrast_txt',
     author: 'a:nth-child(5).xcontrast_txt',
     description: '#profile_top > div',
-    info: 'span.xgray'
-/*     link: '.btn ' */
-  }
-])
-.then((result) => {
-  let paragraph = result[0].info.split(' - ')
-  paragraph.map(chunk => {
-    if (chunk.match( /Chapters:/g )) {
-      let chapts = chunk.replace('Chapters: ', '')
-      console.log(parseInt(chapts))
-    } else {
-      return
+    info: 'span.xgray',
+    body: 'div.storytext@html'
+  }])
+  .then((result) => {
+/*     console.log(result) */
+    let numberOfChapters = 0
+    let paragraph = result[0].info.split(' - ')
+    paragraph.map(chunk => {
+      if (chunk.match(/Chapters:/g)) {
+        let chapts = chunk.replace('Chapters: ', '')
+        let numChapters = parseInt(chapts)
+        numberOfChapters = numChapters
+      } else {
+        return
+      }
+    })
+
+    for (let i = 1; i < (numberOfChapters + 1); i++) {
+      x(`https://www.fanfiction.net/s/7750443/${i}/`, '#content_wrapper_inner', [{
+        body: 'div.storytext@html'
+      }])
+      .then(capitulos => {
+        console.log(i)
+      })
     }
+
   })
-}).catch((err) => {
-  console.error(err)
-});
+  .catch((err) => {
+    console.error(err)
+  });
+
+/*
+No va a servir paginate porque no tiene direccion. No es un link, es un change location
+Necesito hacer el scrape chapters numero de veces.
+*/
